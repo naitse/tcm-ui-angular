c68ca179d02e7b1a4a104adcb73bc099d5fbdd42
naitse <sebastian.gramano@mulesorce.com>
2013-10-29 14:54:07 -0300

    [in progress] manager

--- app/js/ManagerCntl.js
+++ app/js/ManagerCntl.js
@@ -2,6 +2,8 @@
 
 function ManagerCntl($scope, $routeParams, $http, $rootScope, tcm_model) {
 
+	// $scope.tcmItems = tcm_model
+
 
 }
 ManagerCntl.$inject = ['$scope', '$routeParams', '$http', '$rootScope', 'tcm_model'];
