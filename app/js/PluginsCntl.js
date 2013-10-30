fa4d20670b9859ef889170a048fde8a36531c71f
adcarabajal <adcarabajal@gmail.com>
2013-10-30 16:00:57 -0300

    services resoureces refactor

--- app/js/PluginsCntl.js
+++ app/js/PluginsCntl.js
@@ -2,53 +2,8 @@
 
 function PluginsCntl( $scope, $routeParams, $http, $rootScope, tcm_model) {
 
-    $scope.plugins = {
-        'jira': {
-            'id': null,
-            'enabled': false,
-            'props': null
-        }
-    }
+    $scope.plugins = tcm_model.ProjectPlugins.get();
 
-
-    tcm_model.getProjectPlugins($rootScope.currentProject, function(data){
-        var jiraData = [];
-
-        data.forEach(function(elmnt, index){
-            if(elmnt.name === 'Jira' ){
-
-                $scope.plugins.jira.id = elmnt.id
-
-                if(elmnt.prop === 'enabled'){
-                    $scope.plugins.jira.enabled = (elmnt.value === '1')? true:false;
-                }else{
-                    jiraData.push({
-                        'prop': elmnt.prop,
-                        'value': elmnt.value
-                    });
-                }
-
-            }
-
-        })
-
-        $scope.plugins.jira.props = jiraData;
-
-    });
-
-
-    $scope.saveJira = function(){
-
-        $scope.plugins.jira.props.forEach(function(elemnt, index){
-
-            tcm_model.updatePluginProperty($scope.plugins.jira.id, elemnt.prop, elemnt.value, function(){
-
-            }, function(){
-                console.log('error');
-            });
-        })
-
-    };
 
 }
 PluginsCntl.$inject = [ '$scope', '$routeParams', '$http', '$rootScope', 'tcm_model'];
--- app/js/apiModel.js
+++ app/js/apiModel.js
@@ -1,72 +1,32 @@
 tcmModule.service('tcm_model', ['$resource', '$http', '$routeParams', 'Auth', '$cookieStore', function($resource, $http, $routeParams, Auth, $cookieStore) {
 
-     var Projects = $resource(basePath + 'api/projects', {}, {
-         getProj: { method: "GET",
-             withCredentials:true
-         }
-
-     })
-
-    //$http.defaults.headers.common['connect.sess'] = document.cookie.replace('connect.sess=', '');
-    //$http.defaults.withCredentials = true;
-
-    //$http.defaults.headers.common['Cookie'] = document.cookie;
-
 
     return {
-        /*getResource: $resource(basePath + 'api/projects', {}, {
-                getProj: { method: "GET",
-                    withCredentials:true
-                }
+        Profile: $resource(basePath + 'api/profile'),
+        Projects:  $resource(basePath + 'api/projects', {},{
+                    update: { method: 'PUT'}
 
-        }),*/
-        getProfile: function(success, error){
-            $http.get( basePath + 'api/profile').success(success).error(error);
-        },
-        getProjects: function(success, error){
-            //Projects.getProj(success);
-            //console.log(Projects.getProj());
-            $http.get( basePath + 'api/projects').success(success).error(error);
-        },
-        getProjectConfig: function( success, error){
-            $http.get( basePath + 'api/projects/'+$routeParams.projectId+'/config').success(success).error(error);
-        },
-        updateProject: function(projectId, requestBody, success, error){
-            $http.put( basePath + 'api/projects/'+projectId, requestBody).success(success).error(error);
-        },
-        getProjectPlugins: function(projectId, success, error){
-            $http.get( basePath + 'api/projects/'+$routeParams.projectId + '/plugins').success(success).error(error);
-        },
-        updatePluginProperty: function(pluginId, prop, value, success, error){
-            $http.put( basePath + 'api/projects/'+$routeParams.projectId + '/plugins/'+ pluginId, {'prop': prop, 'value': value}).success(success).error(error);
-        },
-        getReleases: function(success, error){
-            $http.get( basePath + 'api/projects/'+$routeParams.projectId+'/releases').success(success).error(error);
-        },
-        getIterations: function(id, success, error){
-            $http.get( basePath + 'api/releases/'+id+'/iterations').success(success).error(error);
-        },
-        createRelease: function(releaseName, success, error){
-            $http.post( basePath + 'api/projects/'+$routeParams.projectId+'/releases', {'name': releaseName}).success(success).error(error);
-        },
-        deleteRelease: function(releaseId, success, error){
-            $http.delete( basePath + 'api/releases/' + releaseId).success(success).error(error);
-        },
-        createIteration: function(releaseId, name, success, error){
-            $http.post( basePath + 'api/releases/' + releaseId + '/iterations', {'name': name}).success(success).error(error)
-        },
-        deleteIteration: function(iterId, success, error){
-            $http.delete( basePath + 'api/iterations/' + iterId).success(success).error(error);
-        },
-        updateIteration: function(iterId, name, success, error){
-            $http.put( basePath + 'api/iterations/' + iterId, {"name": name}).success(success).error(error);
-        },
-        updateRelease: function(rlsId, name, success, error){
-            $http.put( basePath + 'api/releases/' + rlsId, {"name": name}).success(success).error(error);
-        },
-        updateProjectConfig: function(config, success, error){
-            $http.put(basePath + 'api/projects/' + $routeParams.projectId + '/config', config).success(success).error(error);
-        },
+        }),
+        ProjectConfig: $resource(basePath + 'api/projects/:id/config', {id:$routeParams.projectId}, {
+            update: { method: 'PUT'}
+
+        }),
+        ProjectPlugins: $resource(basePath + 'api/projects/:id/plugins/:pid', {id: $routeParams.projectId},{
+            update: { method: 'PUT'}
+
+        }),
+
+        Releases: $resource(basePath + 'api/projects/:projectId/releases/:id', {projectId: $routeParams.projectId, id:'@id'}, {
+            update: { method: 'PUT'}
+
+        }),
+
+        Iterations: $resource(basePath + 'api/releases/:releaseId/iterations/:IterId', {releaseId: '@releaseId', IterId: '@IterId'},{
+                update: {
+                    method: 'PUT'
+                }
+        })
+        /*
         admin: {
             getProjects: function(success, error){
                 $http.get( basePath + 'api/admin/projects').success(success).error(error);
@@ -105,6 +65,6 @@ tcmModule.service('tcm_model', ['$resource', '$http', '$routeParams', 'Auth', '$
             deleteUser: function(id, success, error){
                 $http.delete(basePath + 'api/admin/users/' + id).success(success).error(error);
             }
-        }
+        }  */
     };
 }]);
