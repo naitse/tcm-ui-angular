89ebca3ac3d099334e021ced30b8305ccf7d2564
adcarabajal <adcarabajal@gmail.com>
2013-11-05 16:56:16 -0300

    jira

--- app/js/JiraPluginCntl.js
+++ app/js/JiraPluginCntl.js
@@ -3,23 +3,21 @@
 function JiraPluginCntl( $scope, $routeParams, $http, $rootScope, tcm_model) {
 
     $scope.iterations = tcm_model.JiraIterations.get();
+    $scope.iteration = null;
 
-    $scope.$watch('iteration', function(value){
+    $scope.getIssues = function(value){
         if(value != null){
             $scope.issues= tcm_model.JiraIssues.get({id: value});
         }
 
-    });
+    };
+
+    $scope.releasesIterations = tcm_model.ReleasesIterations.query();
 
 
-   $scope.getPopoverTitle = function(issue){
-       console.log(issue)
-       popoverTitle = issue.key + ': ' + issue.summary;
-   }
-
-
-   $scope.releasesIterations = tcm_model.ReleasesIterations.query();
-
-
+    $scope.syncronize = function(){
+        console.log($scope.issues);
+        console.log($scope.releasesIterations)
+    }
 }
 JiraPluginCntl.$inject = [ '$scope', '$routeParams', '$http', '$rootScope', 'tcm_model'];
