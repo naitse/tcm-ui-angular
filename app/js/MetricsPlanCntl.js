e64cb16e8214f015630201b6b33f549d3c68211c
adcarabajal <adcarabajal@gmail.com>
2013-11-11 18:09:56 -0300

    fix for selection

--- app/js/MetricsPlanCntl.js
+++ app/js/MetricsPlanCntl.js
@@ -1,10 +1,13 @@
 
 
 function MetricsPlanCntl( $scope, $routeParams, $window, tcm_model) {
-
-    $scope.getPlan = function(id){
-        $scope.plan = tcm_model.metrics.Plan.query({id: id});
+    $scope.selection = {
+        iteration: null
     }
+
+    $scope.$watch('selection.iteration', function(id){
+        $scope.plan = tcm_model.metrics.Plan.query({id: id});
+    });
 
     $scope.openHotLink = function(id){
         $window.open('#/metrics/plan/' + $routeParams.projectId + '/' + id);
@@ -14,7 +17,7 @@ function MetricsPlanCntl( $scope, $routeParams, $window, tcm_model) {
         $scope.selectedIteration = null;
         $scope.releasesIterations = tcm_model.ReleasesIterations.query();
     }else{
-        $scope.getPlan($routeParams.iterationId);
+        $scope.selection.iteration = $routeParams.iterationId;
     }
 
 }
