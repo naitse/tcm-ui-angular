320b56a9fb03a95e7241c9a2982dcd4138908501
adcarabajal <adcarabajal@gmail.com>
2013-11-14 12:23:27 -0300

    fix for plan

--- app/js/MetricsPlanCntl.js
+++ app/js/MetricsPlanCntl.js
@@ -6,7 +6,12 @@ function MetricsPlanCntl( $scope, $routeParams, $window, tcm_model) {
     }
 
     $scope.$watch('selection.iteration', function(id){
-        $scope.plan = tcm_model.metrics.Plan.query({id: id});
+        if(id != null){
+            $scope.plan = tcm_model.metrics.Plan.query({id: id});
+        }else{
+            $scope.plan=null;
+        }
+
     });
 
     $scope.openHotLink = function(id){
