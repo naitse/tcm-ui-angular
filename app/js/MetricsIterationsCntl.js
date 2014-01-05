c249a94772a11c77e645597b5c95bb1ef0584607
naitse <sebastian.gramano@mulesorce.com>
2014-01-05 00:11:21 -0300

    [P] fixing metrics

--- app/js/MetricsIterationsCntl.js
+++ app/js/MetricsIterationsCntl.js
@@ -9,14 +9,17 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
         {id: 0, name: 'Not Run'},
         {id: 1, name: 'In Progress'},
         {id: 2, name: 'Blocked'},
-        {id: 3, name: 'Failed'},
+        {id: 3, name: 'Fail'},
         {id: 4, name: 'Pass'}];
 
 
     $scope.details = false;
 
+    var colors = [];
+
     $scope.$watch('selection.iteration', function(id){
         if(id!= null){
+
             $scope.loadExecuted(id);
             $scope.loadDaily(id);
         }
@@ -54,15 +57,29 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
          tcm_model.metrics.Executed.query({id: id}, function(metricsExecuted){
              if(metricsExecuted.length > 0){
                  var chartData = new Array();
+                 colors =[];
+                 //var colors = ['#c6c6c6', '#46ACCA', '#FAA328',  '#CD433D', '#5DB95D'];
+                 
 
                  $scope.title =  {
                      text: metricsExecuted[0].iterName
                  }
 
                  delete metricsExecuted[0]['iterName'];
-                 _.each(metricsExecuted[0], function(value, key){
-                     chartData.push(new Array( key, value));
-                 });
+
+                 if(metricsExecuted[0]['total'] == 0){
+                    chartData.push(new Array( 'Total', 0));
+                 }else{
+                    delete metricsExecuted[0]['total'];
+                     _.each(metricsExecuted[0], function(value, key){
+                        if(value != 0){
+                         chartData.push(new Array( getSerieName(key), value));
+                         colors.push(getColor(key))
+                        }
+                     });
+                 }
+
+                $scope.executed = $scope.compliteChart();
 
                 $scope.executed.series = [{
                      type: 'pie',
@@ -92,7 +109,9 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
         }
     }
 
-    $scope.executed = {
+    $scope.compliteChart = function(){
+
+        return {
         options: {
             chart: {
                 plotBackgroundColor: null,
@@ -103,7 +122,7 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
                 height:200,
                 width:300
             },
-            colors: ['#c6c6c6', '#CD433D', '#5DB95D', '#46ACCA', '#FAA328'],
+            colors: colors,
             title: {
                 text: ""
             },
@@ -131,11 +150,9 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
 
                                 $scope.details = true;
                                 var seriesName = this.name;
-                                var status = _.find($scope.statuses, function(item){
-                                    return item.name === seriesName;
-                                });
+                                var st = _.findWhere($scope.statuses, {name: seriesName});
 
-                                $scope.detailsByStatus = tcm_model.metrics.TCsByStatus.query({'id': $scope.selection.iteration, 'statusId': status.id})
+                                $scope.detailsByStatus = tcm_model.metrics.TCsByStatus.query({'id': $scope.selection.iteration, 'statusId': st.id})
 
                             }
                         }
@@ -144,6 +161,10 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
             }
         }
     }
+
+    }
+
+    $scope.executed = $scope.compliteChart();
 
     $scope.openHotLink = function(id){
         $window.open('#/metrics/iterations/' + $routeParams.projectId + '/' + id);
@@ -160,6 +181,56 @@ function MetricsIterationsCntl( $scope, $routeParams, $window, tcm_model) {
         $scope.releasesIterations = tcm_model.ReleasesIterations.query();
     }else{
         $scope.selection.iteration = $routeParams.iterationId;
+    }
+
+    function getColor(key){
+
+        var color = '';
+        switch(true){
+            case key == 'notrun':
+                color =  '#c6c6c6';
+                break;
+            case key == 'inprogress':
+                color = '#46ACCA';
+                break;
+            case key == 'blocked':
+                color = '#FAA328';
+                break;
+            case key == 'failed':
+                color = '#CD433D';
+                break;
+            case key == 'pass':
+                color = '#5DB95D';
+                break;                
+        }
+
+        return color;
+
+    }
+
+        function getSerieName(key){
+
+        var name = '';
+        switch(true){
+            case key == 'notrun':
+                name =  'Not Run';
+                break;
+            case key == 'inprogress':
+                name =  'In Progress';
+                break;
+            case key == 'blocked':
+                name =  'Blocked';
+                break;
+            case key == 'failed':
+                name =  'Fail';
+                break;
+            case key == 'pass':
+                name =  'Pass';
+                break;                
+        }
+
+        return name;
+
     }
 
 }
