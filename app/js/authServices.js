eddb126e906f5d234b28e00d07bf5d01d5c1baa9
naitse <sebastian.gramano@mulesorce.com>
2014-06-16 18:04:57 -0300

    sync

--- app/js/authServices.js
+++ app/js/authServices.js
@@ -1,5 +1,5 @@
-var basePath = "http://tcm.test-lab.ch:7777/";
-//var  basePath =  "http://ec2-54-197-79-64.compute-1.amazonaws.com:7777/";
+//var basePath = "http://tcm.noip.me:7777/";
+var  basePath =  "http://ec2-54-197-79-64.compute-1.amazonaws.com:7777/";
 //var  basePath =  "http://localhost:7777/";
 
 tcmModule.service('Auth', ['$http', '$cookieStore' , function($http, $cookieStore){
